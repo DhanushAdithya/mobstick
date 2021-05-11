f59b9b1c14f2ba6614898a3848b7792e6732f2dc
Dhanush Adithya <s.dhanushadithya@gmail.com>
2021-05-11 21:31:48 +0530

    Added if block msg

--- public/desktop/app.js
+++ public/desktop/app.js
@@ -1,6 +1,7 @@
 const socket = io()
 
 if (isPhone()) {
+	console.log('Not a phone')
 } else {
 	socket.on('mobile orientation', data => {
 		document.querySelector('.box').style.translate = `${data}px`
